bd39f05ebfb777b9d4ba531b37a76b40ac032099
elainapolson <elainapolson@gmail.com>
2015-06-05 08:22:17 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
-end
+  new_string = ""
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else 
+    array.each do |word|
+      if word != array[-1]
+        new_string += "#{word}, "
+      else 
+        new_string += "and #{word}"
+      end 
+    end 
+    return new_string
+  end
+end 
